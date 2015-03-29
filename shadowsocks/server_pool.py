aa58a65c10e8c89d4c2dec59bd48c9632cab355e
Mujj <q236588129@gmail.com>
2015-03-30 05:52:36 +0800

    开启UDP支持
    开启UDP支持

--- shadowsocks/server_pool.py
+++ shadowsocks/server_pool.py
@@ -48,8 +48,8 @@ class ServerPool(object):
         self.mgr = asyncmgr.ServerMgr()
         self.tcp_servers_pool = {}
         self.tcp_ipv6_servers_pool = {}
-        #self.udp_servers_pool = {}
-        #self.udp_ipv6_servers_pool = {}
+        self.udp_servers_pool = {}
+        self.udp_ipv6_servers_pool = {}
 
         self.loop = eventloop.EventLoop()
         thread.start_new_thread(ServerPool._loop, (self.loop, self.dns_resolver, self.mgr))
@@ -107,9 +107,9 @@ class ServerPool(object):
                     tcp_server = tcprelay.TCPRelay(a_config, self.dns_resolver, False)
                     tcp_server.add_to_loop(self.loop)
                     self.tcp_servers_pool.update({port: tcp_server})
-                    #udp_server = udprelay.UDPRelay(a_config, self.dns_resolver, False)
-                    #udp_server.add_to_loop(self.loop)
-                    #self.udp_servers_pool.update({port: udp_server})
+                    udp_server = udprelay.UDPRelay(a_config, self.dns_resolver, False)
+                    udp_server.add_to_loop(self.loop)
+                    self.udp_servers_pool.update({port: udp_server})
                 except Exception, e:
                     logging.warn(e)
 
